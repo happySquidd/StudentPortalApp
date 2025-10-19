7da7d6d15ee4bbfb2541dfb9e4417e52c3724400
happySquidd <rafael.safin077@gmail.com>
2025-10-18 21:34:27 -0400

    MAUI added

---
+++ StudentPortalApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace StudentPortalApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
