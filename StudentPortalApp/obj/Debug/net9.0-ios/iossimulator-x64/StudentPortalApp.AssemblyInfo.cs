fc38788649cf1281edf28121e8d93f9d17a02d3d
happySquidd <rafael.safin077@gmail.com>
2025-10-25 02:02:39 -0400

    minor fix with buttons

--- StudentPortalApp/obj/Debug/net9.0-ios/iossimulator-x64/StudentPortalApp.AssemblyInfo.cs
+++ StudentPortalApp/obj/Debug/net9.0-ios/iossimulator-x64/StudentPortalApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c338082da4df69eb1b65378eef7e3f7e8fbcfc6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ab8be6318401d89e4e994b0c4ff5635f5bef20cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
