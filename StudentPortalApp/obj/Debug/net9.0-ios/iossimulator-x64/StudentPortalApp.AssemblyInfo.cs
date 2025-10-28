415389125688f0db9efa8f6de9393952cc48227a
happySquidd <rafael.safin077@gmail.com>
2025-10-27 20:33:13 -0400

    minor ui change

--- StudentPortalApp/obj/Debug/net9.0-ios/iossimulator-x64/StudentPortalApp.AssemblyInfo.cs
+++ StudentPortalApp/obj/Debug/net9.0-ios/iossimulator-x64/StudentPortalApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e2a0cb3f6acfa16bc622b5b9e4a6db4c1e35f12b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7f8c6b27e538f8d20bdf27da2048fe2161748375")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
