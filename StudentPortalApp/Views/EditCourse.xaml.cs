91222cd13f9f5a7216df8b00c16cba612d258131
happySquidd <rafael.safin077@gmail.com>
2025-10-27 23:11:13 -0400

    completed edit course page

--- StudentPortalApp/Views/EditCourse.xaml.cs
+++ StudentPortalApp/Views/EditCourse.xaml.cs
@@ -1,10 +1,12 @@
 using StudentPortalApp.Models;
+using StudentPortalApp.Services;
 
 namespace StudentPortalApp.Views;
 
 public partial class EditCourse : ContentPage
 {
-	public EditCourse(Course course)
+	private readonly Course course;
+    public EditCourse(Course course)
 	{
 		InitializeComponent();
 
@@ -15,5 +17,82 @@ public partial class EditCourse : ContentPage
 		InstName.Text = course.InstructorName;
 		InstPhone.Text = course.InstructorPhone;
 		InstEmail.Text = course.InstructorEmail;
+
+		this.course = course;
+	}
+
+	private async void SaveClick(object sender, EventArgs e)
+	{
+		// input validation
+		if (string.IsNullOrWhiteSpace(CourseName.Text))
+		{
+			await DisplayAlert(
+				title: "Error", 
+				message: "Course name cannot be empty", 
+				cancel: "OK");
+			return;
+        }
+		if (CourseEnd.Date <= CourseStart.Date)
+		{
+			await DisplayAlert(
+				title: "Error",
+				message: "The end date cannot be earlier or the same as the start date",
+				cancel: "OK");
+			return;
+        }
+		if (CourseStatus.SelectedItem == null)
+		{
+			await DisplayAlert(
+				title: "Error",
+				message: "Please select a course status",
+				cancel: "OK");
+			return;
+        }
+		if (string.IsNullOrWhiteSpace(InstName.Text) || string.IsNullOrWhiteSpace(InstPhone.Text))
+		{
+			await DisplayAlert(
+				title: "Error",
+				message: "Instructor name and phone cannot be empty",
+				cancel: "OK");
+			return;
+		}
+		if (string.IsNullOrWhiteSpace(InstEmail.Text) || !InstEmail.Text.Contains("@"))
+		{
+			await DisplayAlert(
+				title: "Error",
+				message: "Please enter a valid instructor email",
+				cancel: "OK");
+			return;
+        }
+
+        // get data from fields
+        string courseName = CourseName.Text;
+		DateTime start = CourseStart.Date;
+		DateTime end = CourseEnd.Date;
+		string status = CourseStatus.SelectedItem.ToString();
+		string instructorName = InstName.Text;
+		string instructorPhone = InstPhone.Text;
+		string instructorEmail = InstEmail.Text;
+
+		await DatabaseService.UpdateCourse(
+			id: course.Id,
+			termId: course.TermId,
+			name: courseName,
+			startTime: start.ToString(),
+			endTime: end.ToString(),
+			status: status,
+			instructorName: instructorName,
+			instructorPhone: instructorPhone,
+			instructorEmail: instructorEmail,
+			notes: course.Notes,
+			startNotification: course.StartNotification,
+			endNotification: course.EndNotification,
+			dueDate: course.DueDate);
+		await Navigation.PopAsync();
+    }
+
+	private async void CancelClick(object sender, EventArgs e)
+	{
+		await Navigation.PopAsync();
 	}
 }
--- StudentPortalApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/StudentPortalApp.AssemblyInfo.cs
+++ StudentPortalApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/StudentPortalApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7cb3a7a0691f2b0178c67957331d324bc57b3336")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+040d15b8874051954e5ab92f23b09e5873ced6d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
