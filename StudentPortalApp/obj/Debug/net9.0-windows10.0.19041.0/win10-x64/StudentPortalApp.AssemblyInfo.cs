7107f106b91dc50f079d39f8b6c363d4e98b4fee
happySquidd <rafael.safin077@gmail.com>
2025-12-09 20:53:23 -0500

    removed .vs

--- StudentPortalApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/StudentPortalApp.AssemblyInfo.cs
+++ StudentPortalApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/StudentPortalApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+29795e1d741106937c9b7d7101f978d12b7ebe1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+918b34da26d306aa3c948d27c17c3e422cc8044e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
